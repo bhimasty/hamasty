bc3289c98b2de7f28bd02078f35c0cd7d11fb4a8
pashabhimasty <bhimastypasha@gmail.com>
2021-08-08 07:37:47 +0700

    Layouting Dashboard Admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('pages.admin.dashboard');
+// });
+
+Route::prefix('dashboard')
+    ->namespace('Admin')
+    ->group(function () {
+        Route::get('/', 'DashboardController@index')->name('dashboard');
+    });
